9fa51d9beaf34370e27fffa61b443ea8454a3523
karmenwang <karmenwang1019@gmail.com>
2020-12-02 18:01:43 -0800

    figured out an algorithm for 2d ring buffer data sorter

--- Object_Detection/Unit_Tests/Ring_Buffer.py
+++ Object_Detection/Unit_Tests/Ring_Buffer.py
@@ -1,26 +1,28 @@
-import numpy as np
 from numpy_ringbuffer import RingBuffer
-
 count = 0
 count1 = 2
 r = RingBuffer(capacity=5, dtype=int)
 length = 2
 ring_buffer_2d = []
 
-while count < 10:
-    for i in range(0, length):
-        ring_buffer_2d.append(r)
+while True:
+    created = False
+    if not created:
+        for i in range(0, length):
+            ring_buffer_2d.append(RingBuffer(capacity=5, dtype=int))
+        created = True
 
-    for i in range(0, length):
-        if i == 1:
-            ring_buffer_2d[i].append(count1)
-            count1 = count1*2
-        else:
-            ring_buffer_2d[i].append(count)
-
+    while count < 10:
+        for i in range(0, length):
+            if i == 1:
+                ring_buffer_2d[i].append(count1)
+                count1 = count1*2
+            else:
+                ring_buffer_2d[i].append(count)
+        count += 1
         print(ring_buffer_2d)
 
     ring_buffer_2d.clear()
 
-    count += 1
+
 
