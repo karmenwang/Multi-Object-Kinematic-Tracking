73eda6bcb5fc669d06f03b05dc4f71256de95e5d
karmenwang <karmenwang1019@gmail.com>
2020-12-03 16:46:40 -0800

    implemented a ring buffer for each detected object's ring buffers

--- Object_Detection/Unit_Tests/Ring_Buffer.py
+++ Object_Detection/Unit_Tests/Ring_Buffer.py
@@ -4,25 +4,24 @@ count1 = 2
 r = RingBuffer(capacity=5, dtype=int)
 length = 2
 ring_buffer_2d = []
+created = False
 
-while True:
-    created = False
+while count < 20:
     if not created:
         for i in range(0, length):
             ring_buffer_2d.append(RingBuffer(capacity=5, dtype=int))
         created = True
 
-    while count < 10:
-        for i in range(0, length):
-            if i == 1:
-                ring_buffer_2d[i].append(count1)
-                count1 = count1*2
-            else:
-                ring_buffer_2d[i].append(count)
-        count += 1
-        print(ring_buffer_2d)
+    for i in range(0, length):
+        if i == 1:
+            ring_buffer_2d[i].append(count1)
+            count1 = count1*2
+        else:
+            ring_buffer_2d[i].append(count)
+    print(str(ring_buffer_2d[0]._left_index) + " " +str(ring_buffer_2d[0]._right_index))
+    count += 1
+    print(ring_buffer_2d[0])
 
-    ring_buffer_2d.clear()
 
 
 
--- Object_Detection/main.py
+++ Object_Detection/main.py
@@ -1,8 +1,7 @@
 # package imports (go to file > settings > project > python interpreter > + in top right > add the following:
 import cv2  # opencv_python
-import time
 from numpy_ringbuffer import RingBuffer
-import numpy as np
+import time
 
 # file imports
 from Classes.CentroidTracker import CentroidTracker
@@ -15,11 +14,11 @@ import Classes.MovingAverage as MovingAverage
 LINE_COORD = [[600, 0], [600, 700]]  # [[x1,y1], [x2, y2]]
 SAMPLE_SIZE = 5
 TIME_INTERVAL = 0.000001
-objects_2D_ring_buffer_x = []
-objects_2D_ring_buffer_y = []
-objects_2D_ring_buffer_t = []
-xPosSampleArray = RingBuffer(capacity=SAMPLE_SIZE, dtype=int)
-yPosSampleArray = RingBuffer(capacity=SAMPLE_SIZE, dtype=int)
+objects_2D_ring_buffer_x = RingBuffer(capacity=4, dtype=RingBuffer)
+objects_2D_ring_buffer_y = RingBuffer(capacity=2, dtype=RingBuffer)
+objects_2D_ring_buffer_t = RingBuffer(capacity=2, dtype=RingBuffer)
+created_2d_ring_buffer = False
+past_objects = 0
 
 # Create new object #############
 image = VideoSetUp.IMGProcess()
@@ -50,56 +49,62 @@ while True:
     cv2.line(imgContour, (LINE_COORD[0][0], LINE_COORD[0][1]), (LINE_COORD[1][0], LINE_COORD[1][1]), (0, 0, 255), 2)
 
     objects = ct.update(bounding_box_array)
+    # print(objects)
 
-    # loop over the tracked objects
-    for objectID in range(0, len(bounding_box_array)):
-        timeSampleArray = [time.time()] * SAMPLE_SIZE
+    if not created_2d_ring_buffer:
+        for objectID in range(0, len(bounding_box_array)):
+            # timeSampleArray = [time.time()] * SAMPLE_SIZE
 
-        objects_2D_ring_buffer_x.append(xPosSampleArray)
-        objects_2D_ring_buffer_y.append(yPosSampleArray)
-        objects_2D_ring_buffer_t.append(timeSampleArray)
+            objects_2D_ring_buffer_x.append(RingBuffer(capacity=SAMPLE_SIZE, dtype=int))
+            objects_2D_ring_buffer_y.append(RingBuffer(capacity=SAMPLE_SIZE, dtype=int))
+            objects_2D_ring_buffer_t.append(RingBuffer(capacity=SAMPLE_SIZE, dtype=int))
+            created_2d_ring_buffer = True
+        # print(objects_2D_ring_buffer_x)
 
-    # print(objects_2D_ring_buffer_x)
+    try:
+        if len(objects) > past_objects:
+            objects_2D_ring_buffer_x.append(RingBuffer(capacity=SAMPLE_SIZE, dtype=int))
+    except TypeError:
+        print("past_object is None type")
 
     for (objectID, centroid) in objects.items():
-        # draw both the ID of the object and the centroid of the
-        # object on the output frame
-        text = "ID {}".format(objectID)
-        cv2.putText(imgContour, text, (centroid[0] - 10, centroid[1] - 10),
+        # draw both the ID of the object and the centroid
+        cv2.putText(imgContour, "ID {}".format(objectID), (centroid[0] - 10, centroid[1] - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.circle(imgContour, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-        print("should be: " + str(objectID) + " " + str(centroid[0]))
-
+        print("should be: " + str(objectID) + " " + str(centroid[0]) + " " + str(centroid[1]))
         try:
             objects_2D_ring_buffer_x[objectID].append(centroid[0])
             objects_2D_ring_buffer_y[objectID].append(centroid[1])
-            # objects_2D_ring_buffer_t[objectID] = tMovingAverage.ring_buffer(time.time())
-            print("results: " + str(objectID) + " " + str(objects_2D_ring_buffer_x))
+            objects_2D_ring_buffer_t[objectID].append(time.time())
+            print("results X: " + str(objectID) + " " + str(objects_2D_ring_buffer_x))
+            print("results Y: " + str(objectID) + " " + str(objects_2D_ring_buffer_y))
+            print("results Time: " + str(objectID) + " " + str(objects_2D_ring_buffer_t))
 
         except (IndexError, AttributeError):
             print("index out of range")
 
+    if objects_2D_ring_buffer_x._left_index + objects_2D_ring_buffer_x._right_index == objects_2D_ring_buffer_x.maxlen:
+        print("object_2D_ring_buffer has been filled")
+        ct.nextObjectID = 0
+
     # for (objectID, centroid) in objects.items():
-
-        # try:
-        #     objectOnScreen = OnScreenObject.OnScreenObject(SAMPLE_SIZE, objectID)
-        #
-        # except TypeError:
-        #     print("No objects detected")
-        #
-        # else:
-        #     objectOnScreen.determineVectors(centroid=centroid)
-        #     objectSchedule.scheduler.enter(TIME_INTERVAL, 0, objectOnScreen.AverageCalculator)
-
-    if len(bounding_box_array) != 0:
-        objects_2D_ring_buffer_x.clear()
-        objects_2D_ring_buffer_y.clear()
-        objects_2D_ring_buffer_t.clear()
+    #     try:
+    #         objectOnScreen = OnScreenObject.OnScreenObject(SAMPLE_SIZE, objectID)
+    #
+    #     except TypeError:
+    #         print("No objects detected")
+    #
+    #     else:
+    #         objectOnScreen.determineVectors(centroid=centroid)
+    #         objectSchedule.scheduler.enter(TIME_INTERVAL, 0, objectOnScreen.AverageCalculator)
 
     imgStack = VideoSetUp.stack_images(0.8, ([img, imgContour, image.colorMask]))
     # cv2.imshow("Result", imgStack)
     cv2.imshow("Contour", imgContour)
+
+    past_objects = len(objects)
 
     if cv2.waitKey(1) & 0xff == ord('q'):  # press q to exit
         break
